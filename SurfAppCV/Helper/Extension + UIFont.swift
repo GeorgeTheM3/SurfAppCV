30b01687c69a8cb68099d4649bd1be0b05d4d11a
GeorgeTheM3 <99905582+GeorgeTheM3@users.noreply.github.com>
2023-08-01 22:25:40 +0300

    fix font

--- SurfAppCV/Helper/Extension + UIFont.swift
+++ SurfAppCV/Helper/Extension + UIFont.swift
@@ -9,17 +9,17 @@ import UIKit
 
 extension UIFont {
     static let helvetica14: UIFont = {
-        guard let font = UIFont(name: "Helvetica", size: 14) else { return UIFont() }
+        guard let font = UIFont(name: "Helvetica-Light", size: 14) else { return UIFont.systemFont(ofSize: 14) }
         return font
     }()
     
     static let helvetica16: UIFont = {
-        guard let font = UIFont(name: "Helvetica", size: 16) else { return UIFont() }
+        guard let font = UIFont(name: "Helvetica-Regular", size: 16) else { return UIFont.systemFont(ofSize: 16)}
         return font
     }()
     
     static let helvetica25bold: UIFont = {
-        guard let font = UIFont(name: "Helvetica-Bold", size: 25) else { return UIFont() }
+        guard let font = UIFont(name: "Helvetica-Bold", size: 25) else { return UIFont.systemFont(ofSize: 25) }
         return font
     }()
 }
