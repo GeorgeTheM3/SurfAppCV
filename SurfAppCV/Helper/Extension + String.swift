aa70b40d429d53756d64a796ab479c7eaf44676d
GeorgeTheM3 <99905582+GeorgeTheM3@users.noreply.github.com>
2023-08-01 17:36:46 +0300

    extension string

---
+++ SurfAppCV/Helper/Extension + String.swift
@@ -0,0 +1,14 @@
+//
+//  Extension + String.swift
+//  SurfAppCV
+//
+//  Created by Георгий Матченко on 01.08.2023.
+//
+
+import UIKit
+
+extension String {
+    func getTextWidth(_ string: String) -> CGFloat {
+        return NSString(string: string).boundingRect(with: CGSize(width: 0, height: 0), options: .usesFontLeading, attributes: [.font: UIFont.helvetica14], context: nil).width
+    }
+}
