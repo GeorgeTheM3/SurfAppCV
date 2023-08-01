1d2c7d55288c043e736679f6b20b178c3428ad46
GeorgeTheM3 <99905582+GeorgeTheM3@users.noreply.github.com>
2023-08-01 14:25:22 +0300

    add new font extension uifont

--- SurfAppCV/Helper/Extension + UIFont.swift
+++ SurfAppCV/Helper/Extension + UIFont.swift
@@ -13,6 +13,11 @@ extension UIFont {
         return font
     }()
     
+    static let helvetica16: UIFont = {
+        guard let font = UIFont(name: "Helvetica", size: 16) else { return UIFont() }
+        return font
+    }()
+    
     static let helvetica25bold: UIFont = {
         guard let font = UIFont(name: "Helvetica-Bold", size: 25) else { return UIFont() }
         return font
