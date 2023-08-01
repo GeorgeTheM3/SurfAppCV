c0fcff213f4c6dd6ffb01adcd3e4c5e2282ef695
GeorgeTheM3 <99905582+GeorgeTheM3@users.noreply.github.com>
2023-08-01 15:16:51 +0300

    class LeftAlignedCollectionViewFlowLayout

---
+++ SurfAppCV/Helper/LeftAlignmentCollection.swift
@@ -0,0 +1,29 @@
+//
+//  LeftAlignmentCollection.swift
+//  SurfAppCV
+//
+//  Created by Георгий Матченко on 01.08.2023.
+//
+
+import UIKit
+
+class LeftAlignedCollectionViewFlowLayout: UICollectionViewFlowLayout {
+    
+    override func layoutAttributesForElements(in rect: CGRect) -> [UICollectionViewLayoutAttributes]? {
+        let attributes = super.layoutAttributesForElements(in: rect)
+
+        var leftMargin = sectionInset.left
+        var maxY: CGFloat = 0.0
+        attributes?.forEach { layoutAttribute in
+            if layoutAttribute.frame.origin.y >= maxY {
+                leftMargin = sectionInset.left
+            }
+            
+            layoutAttribute.frame.origin.x = leftMargin
+            
+            leftMargin += layoutAttribute.frame.width + minimumInteritemSpacing
+            maxY = max(layoutAttribute.frame.maxY , maxY)
+        }
+        return attributes
+    }
+}
